c2258c050a730c0e03697f37226e249d4863dc74
fengandzhy <fengandzhy@gmail.com>
2023-08-06 22:25:26 +1200

    circle-queue SimpleQueue

---
+++ structure-algorithms/circle-queue/src/main/java/org/zhouhy/structure/algorithms/circle/queue/demo01/Employee.java
@@ -0,0 +1,37 @@
+package org.zhouhy.structure.algorithms.circle.queue.demo01;
+
+public class Employee {
+    int id;
+    String name;
+    int managerId;
+
+    public Employee(int id, String name, int managerId) {
+        this.id = id;
+        this.name = name;
+        this.managerId = managerId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+}
