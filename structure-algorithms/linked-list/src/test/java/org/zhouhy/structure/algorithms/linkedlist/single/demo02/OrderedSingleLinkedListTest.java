87ba0664c57485d69d6b2ec0bc69eaf543982529
fengandzhy <fengandzhy@gmail.com>
2023-08-10 21:25:44 +1200

    linked-list ordered single linked list

--- structure-algorithms/linked-list/src/test/java/org/zhouhy/structure/algorithms/linkedlist/single/demo02/OrderedSingleLinkedListTest.java
+++ structure-algorithms/linked-list/src/test/java/org/zhouhy/structure/algorithms/linkedlist/single/demo02/OrderedSingleLinkedListTest.java
@@ -92,5 +92,39 @@ public class OrderedSingleLinkedListTest {
         orderedSingleLinkedList.add(node1);
 
         assertEquals("呼延灼",orderedSingleLinkedList.getNodeByNumber(8).getName());
+        assertEquals("林冲",orderedSingleLinkedList.getNodeByNumber(6).getName());
+    }
+    
+    @Test
+    public void testDeleteByNumber(){
+        OrderedSingleLinkedList orderedSingleLinkedList = new OrderedSingleLinkedList();
+
+        HeroNode node1 = new HeroNode(1,"宋江", "及时雨");
+        HeroNode node2 = new HeroNode(2,"卢俊义", "玉麒麟");
+        HeroNode node3 = new HeroNode(3,"吴勇", "智多星");
+        HeroNode node4 = new HeroNode(4,"公孙胜", "入云龙");
+        HeroNode node5 = new HeroNode(5,"关胜", "大刀");
+        HeroNode node6 = new HeroNode(6,"林冲", "豹子头");
+        HeroNode node7 = new HeroNode(7,"秦明", "霹雳火");
+        HeroNode node8 = new HeroNode(8,"呼延灼", "双鞭");
+        HeroNode node9 = new HeroNode(9,"花荣", "小李广");
+        HeroNode node10 = new HeroNode(10,"柴进", "小旋风");
+
+        orderedSingleLinkedList.add(node1);
+        orderedSingleLinkedList.add(node2);
+        orderedSingleLinkedList.add(node3);
+        orderedSingleLinkedList.add(node4);
+        orderedSingleLinkedList.add(node5);
+        orderedSingleLinkedList.add(node6);
+        orderedSingleLinkedList.add(node7);
+        orderedSingleLinkedList.add(node8);
+        orderedSingleLinkedList.add(node9);
+        orderedSingleLinkedList.add(node10);
+        
+        assertEquals(10, orderedSingleLinkedList.size());        
+        orderedSingleLinkedList.deleteByNumber(10);
+        assertEquals(9, orderedSingleLinkedList.size());
+        assertEquals("呼延灼",orderedSingleLinkedList.getNodeByNumber(8).getName());
+        assertEquals("关胜",orderedSingleLinkedList.getNodeByNumber(5).getName());
     }
 }
